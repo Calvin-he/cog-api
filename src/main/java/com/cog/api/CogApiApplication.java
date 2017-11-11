aaa0df769c54e6c352910149577779ac54546859
Calvin.He <xiangh@hpe.com>
2017-11-11 22:46:58 +0800

    intial project skeleton

---
+++ src/main/java/com/cog/api/CogApiApplication.java
@@ -0,0 +1,13 @@
+package com.cog.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CogApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CogApiApplication.class, args);
+    }
+
+}
